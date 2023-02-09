91a77ba97be3232a88e2b4f2baa83f470c99594c
Rakesh Mandal <rakeshmandal651@gmail.com>
2023-02-09 21:47:47 +0530

    init

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,20 @@
+import Card from "./Card";
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
+  const expenseDate = props.data.date;
+  const expenseItem = props.data.title;
+  const expensePrice = props.data.amount;
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={expenseDate}></ExpenseDate>
+      <div className="expense-item__description">
+        <h2>{expenseItem}</h2>
+        <div className="expense-item__price">${expensePrice}</div>
+      </div>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
---
+++ src/components/Expenses.js
@@ -0,0 +1,16 @@
+import Card from "./Card";
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+
+function Expenses(props) {
+  return (
+    <Card className="expenses">
+      <ExpenseItem data={props.item[0]}></ExpenseItem>
+      <ExpenseItem data={props.item[1]}></ExpenseItem>
+      <ExpenseItem data={props.item[2]}></ExpenseItem>
+      <ExpenseItem data={props.item[3]}></ExpenseItem>
+    </Card>
+  );
+}
+
+export default Expenses;
